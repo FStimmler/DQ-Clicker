ddf8ce1a8b77cbfd9794420eb6b06f2d8ab4d299
Francisco <franciscostimmler@yahoo.com.ar>
2024-11-23 16:33:59 -0300

    Added Animation

--- js/script.js
+++ js/script.js
@@ -15,10 +15,20 @@ class Monster {
 }
 
 class Hero {
+    static expArray = [0,7, 23, 47, 110, 220, 450, 800, 
+        1300, 2000, 2900, 4000, 5500, 7500, 
+        10000, 13000, 17000, 21000, 25000, 
+        29000, 33000, 37000, 41000, 45000, 
+        49000, 53000, 57000, 61000, 65000, 
+        65535]
+    
     static active = null;
-    constructor(maxlife,attack) {
+    constructor(maxlife,attack,exp,expToLvl,lvl) {
         this.maxlife = maxlife;   // Puntos de vida del Heroe
         this.attack = attack;   // Puntos de ataque del Heroe
+        this.exp = exp; //Experiencia del heroe
+        this.expToLvl = expToLvl //Experiencia necesaria para subir de nivel
+        this.lvl=lvl;
         this.activate();
     }
     
@@ -28,7 +38,28 @@ class Hero {
     
     increaseAttack(n){
         this.attack += n;
+        save();
     }
+
+    increaseExp(xp){
+        this.exp += xp;
+        console.log(this.exp)
+        console.log(this.expToLvl)
+        if(this.exp >=  this.expToLvl){
+            this.lvl++
+            this.expToLvl = Hero.expArray[this.lvl]
+            this.increaseAttack(1)
+            lvlUpAudio.play();
+            levelupText();
+        }
+        expToLvlDisplay.textContent = this.expToLvl;
+        expDisplay.textContent = this.exp;
+        document.title = "DQ-Clicker LVL: " + this.lvl;
+        lvlDisplay.textContent = this.lvl;
+        save();
+    }
+
+
 }
 
 
@@ -62,28 +93,33 @@ function getCookie(name) {
  * 
  */
 let theHero
-const savedData = getCookie('stats') || { lifeMonster: 100,count2: 0, exp: 0, lvl: 1, gold: 0, sword: false,theHero: new Hero(100,10) };
+const savedData = getCookie('stats') || { lifeMonster: 100,count2: 0, exp: 0, lvl: 1, gold: 0, sword: false,theHero: new Hero(100,10,0,7,1) };
 console.log(savedData);
 ({ lifeMonster,count2, exp, lvl, gold, sword,theHero} = savedData);
-theHero = new Hero(theHero.maxlife,theHero.attack)
+theHero = new Hero(theHero.maxlife,theHero.attack,theHero.exp,theHero.expToLvl,theHero.lvl)
 console.log(theHero);
 
 
-document.title = "DQ-Clicker LVL: " + lvl;
 
 // Referencias a los elementos del DOM
 const image = document.getElementById('clickableImage');
 const lifeBarMonster = document.getElementById('lifeBarMonster');
 const lifeBarHero = document.getElementById('lifeBarHero');
+const counterDisplay = document.getElementById('counter');
+const levelUpText = document.getElementById('levelUpText');
 const counterDisplay2 = document.getElementById('clickCount2');
 const expDisplay = document.getElementById('exp');
+const expToLvlDisplay = document.getElementById('expToLvl');
 const lvlDisplay = document.getElementById('lvl');
 const goldDisplay = document.getElementById('gold');
 const buySwordButton = document.getElementById('buySwordButton');
 const lifeBarTextMonster = document.getElementById('lifeBarTextMonster');
 const lifeBarTextHero = document.getElementById('lifeBarTextHero');
+const lvlUpAudio = document.getElementById('lvlUpAudio');
 
+levelUpText.style.display = 'none'
 
+theHero.increaseExp(0)
 const slime = new Monster(0, 'Slime', 100,1);
 const bat = new Monster(1, 'Bat', 150,2);
 const chimaera = new Monster(2,'Chimaera',125,1)
@@ -92,16 +128,15 @@ pvec = [400,400,200] //Vector de probabilidad segun la zona
 setMonster(slime.num);
 
 
-lifeMonster = Monster.active.maxlife
-lifeHero = 100
+lifeMonster = Monster.active.maxlife;
+lifeHero = Hero.active.maxlife;
 updateHealthBars(lifeMonster,lifeHero);
 // Actualizar la visualización inicial
 counterDisplay2.textContent = count2;
-expDisplay.textContent = exp;
-lvlDisplay.textContent = lvl;
+expDisplay.textContent = theHero.exp;
+
 goldDisplay.textContent = gold;
 buySwordButton.disabled = sword || gold < 100;
-
 
 
 // Manejar clics en la imagen interactiva (superior)
@@ -112,34 +147,24 @@ image.addEventListener('click', () => {
     // Verificar si la vida del monstruo llega a 0
     if (lifeMonster <= 0) {
         newEnemy(); //Aparece un nuevo enemigo cuando el anterior muere
-        exp += Math.floor(Math.random() * 9 + 1); // Incrementar EXP con cada clic
+        exp = Math.floor(Math.random() * 9 + 1); // Incrementar EXP con cada clic
         gold += Math.floor(Math.random() * 5); // Incrementar oro con cada clic
-        count2++;   // Incrementa el contador de reinicios
+        count2++;  
         lifeMonster =Monster.active.maxlife; // Reinicia la barra de vida
+        theHero.increaseExp(exp);
     }
-
-    // Verificar si EXP alcanza el límite
-    if (exp >= 100) {
-        exp -= 100; // Restar EXP al máximo
-        lvl++;      // Incrementar nivel
-        document.title = "DQ-Clicker LVL: " + lvl;
-
-    }
-
-    
 
     // Actualizar el botón de comprar espada
     buySwordButton.disabled = sword || gold < 100;
 
     // Guardar datos en la cookie
-    setCookie('stats', { count2, exp, lvl, gold, sword,theHero }, 7); 
+    save()
 
     // Actualizar la visualización
     updateHealthBars(lifeMonster,lifeHero);
     
     counterDisplay2.textContent = count2;
-    expDisplay.textContent = exp;
-    lvlDisplay.textContent = lvl;
+
     goldDisplay.textContent = gold;
     
     //Verifica si la salud del heroe llega a 0
@@ -155,7 +180,7 @@ buySwordButton.addEventListener('click', () => {
         sword = true; 
         buySwordButton.disabled = true; 
         theHero.increaseAttack(5);
-        setCookie('stats', { lifeMonster, count2, exp, lvl, gold, sword,theHero }, 7); 
+        save(); 
         goldDisplay.textContent = gold;
     }
 });
@@ -172,6 +197,19 @@ function setMonster(num) {
     }
 
     Monster.arrayMonsters[num].activate();
+}
+
+function levelupText(){
+    counterDisplay.style.display = 'none';
+    levelUpText.style.display = 'block';
+    levelUpText.classList.add('animated');
+
+    // Eliminar la clase después de 1 segundo para permitir que el efecto se reinicie
+    setTimeout(() => {
+        levelUpText.classList.remove('animated');
+        counterDisplay.style.display = 'block';
+        levelUpText.style.display = 'none';
+    }, 3000);
 }
 
 function newEnemy(){ //Funcion que genera un nuevo enemigo
@@ -217,4 +255,10 @@ function updateHealthBars(lifeMonster,lifeHero){
     lifeBarMonster.style.width = `${(lifeMonster / Monster.active.maxlife * 100)}%`;
     lifeBarTextHero.textContent = `Hero HP:${lifeHero}/${Hero.active.maxlife}`;
     lifeBarHero.style.width = `${(lifeHero / Hero.active.maxlife * 100)}%`;
+}
+
+function save(){
+    theHero = Hero.active
+    console.log(Hero.active)
+    setCookie('stats', { count2, exp, lvl, gold, sword,theHero }, 7); 
 }
